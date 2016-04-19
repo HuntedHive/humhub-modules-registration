976252901b5955ad21380a03e80d90abc74ed95d
Bec Todd <bec@bectodd.com>
2016-04-19 19:51:00 +1000

    TC-6 Update Registration Admin UI for Subject Area
    Update Registration Admin panel UI for subject area select box and
    relation to teacher type.
    https://projects.invisionapp.com/d/main#/console/4365594/142645223/comme
    nts/60868106

--- views/registration/_subjectArea.php
+++ views/registration/_subjectArea.php
@@ -1,15 +1,19 @@
 <?php 
 $flag = HSetting::model()->find('value="'.ManageRegistration::$type[ManageRegistration::TYPE_SUBJECT_AREA]. '"')->value_text;
 ?>
+
+<link rel="stylesheet" type="text/css"
+         href="<?php echo $this->module->assetsUrl; ?>/css/registration.css"/>
+
 <h4>Teacher Subject Area</h4>
 
 <div class="table-responsive">
 	<div class="row no-margin">
-    	<div class="col-xs-5 no-padding">
+    	<div class="col-xs-4 no-padding">
 			<h5><strong>Item Name</strong></h5>                     
     	</div>
-        <div class="col-xs-3 no-padding">
-            <h5><strong>Depend</strong></h5>
+        <div class="col-xs-4 no-padding">
+            <h5><strong>Relates to Teacher Types</strong></h5>
         </div>
         <div class="col-xs-4 no-padding">
             <div class="checkbox regular-checkbox-container pull-right checkbox-required">
@@ -29,7 +33,7 @@ $flag = HSetting::model()->find('value="'.ManageRegistration::$type[ManageRegist
                     echo '<tr><td class="empty"><span class="empty">Add items to the list.</span></td></tr>';
                 } else {
                     foreach ($subjects as $subject) {
-                        echo '<tr class="ui-sortable" data-item="item_'.$subject->id.'"><td style="z-index:99999;"><i class="fa fa-bars dragdrop"></i><span class="m_item" data-pk="' . $subject->id . '" data-url="' . $this->createUrl('edit') . '">'.$subject->name.'</span></td><td>' . ManageRegistration::getDependName($subject->depend) . '</td><td><a class="btn btn-danger btn-xs tt close" href="' . $this->createUrl('delete', ['id' => $subject->id]) . '" title="delete item"><i class="fa fa-times"></i></a></td></tr>';
+                        echo '<tr class="ui-sortable" data-item="item_'.$subject->id.'"><td class="col-sm-4" style="z-index:99999;"><i class="fa fa-bars dragdrop"></i><span class="m_item" data-pk="' . $subject->id . '" data-url="' . $this->createUrl('edit') . '">'.$subject->name.'</span></td><td class="col-sm-6"><span class="label label-success">' . ManageRegistration::getDependName($subject->depend) . '</span></td><td class="col-sm-2"><a class="btn btn-danger btn-xs tt close" href="' . $this->createUrl('delete', ['id' => $subject->id]) . '" title="delete item"><i class="fa fa-times"></i></a></td></tr>';
                     }
                 }
                 ?>
@@ -41,21 +45,26 @@ $flag = HSetting::model()->find('value="'.ManageRegistration::$type[ManageRegist
           
 <div class="form form-registration-items">
     <?php echo CHtml::beginForm(['action' => '', 'method' => 'post']); ?>
-    <?php echo CHtml::activeHiddenField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'type', ['value' => ManageRegistration::TYPE_SUBJECT_AREA]); ?>
-    <?php echo CHtml::activeHiddenField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'default', ['value' => ManageRegistration::DEFAULT_ADDED]); ?>
     <div class="row controls">
-        <div class="col-xs-12">
-                <?php echo CHtml::errorSummary($model[ManageRegistration::TYPE_SUBJECT_AREA], '', ''); ?>
-        </div>
-        <div class="col-sm-6">
-            <?php echo CHtml::activeTextField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'name', array('class' => 'form-control input-sm', 'placeholder' => 'Enter item name',)); ?>
-            <?php echo CHtml::activeDropDownList($model[ManageRegistration::TYPE_SUBJECT_AREA], 'depend', ManageRegistration::getTeachetTypeDropDownList() , array('class' => 'form-control input')); ?>
-            <button type="submit" name="btn" class="btn btn-primary btn-sm">
+
+        <div class="col-md-8 selectpicker-tags">
+            <?php echo CHtml::activeTextField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'name', array('class' => 'form-control input-sm pull-left', 'placeholder' => 'Enter item name',)); ?>
+            <!-- Existing Selectbox <?php echo CHtml::activeDropDownList($model[ManageRegistration::TYPE_SUBJECT_AREA], 'depend', ManageRegistration::getTeachetTypeDropDownList() , array('class' => 'form-control input-sm selectpicker show-tick pull-left')); ?> -->
+            <select name="subjectarea" class="selectpicker form-control show-tick input-sm pull-left" multiple title="Select related teacher type(s)...">
+                <optgroup label="Select related teacher type(s)">
+                  <option  data-content="<span class='label label-success'>primary school</span>">primary school</option>
+                  <option  data-content="<span class='label label-success'>high school</span>">high school</option>
+                  <option  data-content="<span class='label label-success'>other</span>">other</option>
+                </optgroup>
+            </select>
+
+            <button type="submit" name="btn" class="btn btn-primary btn-sm pull-left">
                 <i class="fa fa-plus"></i> add item
             </button>
+            <div class="clearfix"></div>
         </div>
 
-        <div class="col-sm-6">
+        <div class="col-md-4">
         	<div class="checkbox regular-checkbox-container pull-right">
                 <label>
                     <a href='<?= $this->createUrl('type', ['type' => ManageRegistration::TYPE_SUBJECT_AREA]) ?>' data-method='post'>
@@ -65,6 +74,13 @@ $flag = HSetting::model()->find('value="'.ManageRegistration::$type[ManageRegist
                 </label>
                 <div class="regular-checkbox-clear"></div>
             </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+             <?php echo CHtml::errorSummary($model[ManageRegistration::TYPE_SUBJECT_AREA], '', ''); ?>
+             <?php echo CHtml::activeHiddenField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'type', ['value' => ManageRegistration::TYPE_SUBJECT_AREA]); ?>
+             <?php echo CHtml::activeHiddenField($model[ManageRegistration::TYPE_SUBJECT_AREA], 'default', ['value' => ManageRegistration::DEFAULT_ADDED]); ?>
         </div>
     </div>
 	<?php echo CHtml::endForm(); ?>
