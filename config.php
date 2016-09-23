347a1d22e731aa95901ba95ba87f1dedc9aa5c16
VolkovDenis <volkovdenya@gmail.com>
2016-09-23 13:26:04 +0300

    license

--- config.php
+++ config.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.org/licences GNU AGPL v3
+ */
+
 use humhub\modules\admin\widgets\AdminMenu;
 
 return [
